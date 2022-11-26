fd01e53d57e58fed5f946119d596a56f3f868d73
Dawid Krok <privatedawidk@gmail.com>
2022-11-26 10:19:57 +0100

    Display cart content

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,13 @@
+header = `<div><h1>NAME</h1><h1>PRICE</h1><h1>QUANTITY</h1><h1>TOTAL</h1></div>`
+sum = 0
+
 populateProducts = async cart => {
     if(!cart.length)
         return products_container.innerHTML = "<h1>Cart is empty<h1>"
+
+    products_container.innerHTML = header
+
+    sum = 0
 
     for(entry of cart) {
         // get product based on productId
@@ -12,9 +19,16 @@ populateProducts = async cart => {
         node.append(`<p>${entry.quantity}</p>`)
         node.append(`<p>${entry.quantity*p.price}$</p>`)
 
+        sum += entry.quantity*p.price
+
         products_container.append(node[0])
-    
     }
+
+    updateSum()
+}
+
+updateSum = () => {
+    sum_display.innerHTML = `Sum: ${sum}$`
 }
 
 authorizedFetch("getCart")
